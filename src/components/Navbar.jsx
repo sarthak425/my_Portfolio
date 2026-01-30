80a1e193f0450d92f605a73aeb611f7937d04768
sarthak425 <sarthak.khatpe123@gmail.com>
2026-01-30 10:04:26 +0530

    add new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/Portfolio/Resume.pdf';
+    const resumeUrl = '/Resume.pdf';
     window.open(resumeUrl);
   };
 
